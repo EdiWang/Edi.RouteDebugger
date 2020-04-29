8fbafecc5761ea2f294277689fdcc31a686f40c0
Edi Wang <Edi.Wang@outlook.com>
2020-04-29 14:35:42 +0800

    Add code files

---
+++ src/Edi.RouteDebugger/RouteDebuggerExtensions.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Builder;
+
+namespace Edi.RouteDebugger
+{
+    public static class RouteDebuggerExtensions
+    {
+        public static IApplicationBuilder UseRouteDebugger(this IApplicationBuilder app)
+        {
+            app.UseMiddleware<RouteDebuggerMiddleware>();
+            return app;
+        }
+    }
+}
